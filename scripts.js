bd5fff819c057d411d0d77a809dc0d74e151fdd1
Muflih <69343587+mufmufmuf@users.noreply.github.com>
2023-03-18 14:34:34 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,175 +1,69 @@
-let simonPattern = [];
-let playerPattern = [];
-
-let counter = 0;
-
-let greenAudio = new Audio("./sounds/green.mp3");
-greenAudio.volume = 0.1;
-let redAudio = new Audio("./sounds/red.mp3");
-redAudio.volume = 0.1;
-let yellowAudio = new Audio("./sounds/yellow.mp3");
-yellowAudio.volume = 0.1;
-let blueAudio = new Audio("./sounds/blue.mp3");
-blueAudio.volume = 0.1;
-let wrongAudio = new Audio("./sounds/wrong.mp3");
-wrongAudio.volume = 0.1;
-
-let green = $("#green");
-let red = $("#red");
-let yellow = $("#yellow");
-let blue = $("#blue");
-let level = $("#level-title");
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let play = false;
 
 $(document).on("keydown", () => {
-    pick();
+    if (play == false) {
+        gamePattern = [];
+        userClickedPattern = [];
+        nextSequence();
+        play = true;
+    }
 });
 
-function pick() {
-    let random = Math.floor(Math.random() * 4) + 1;
+function nextSequence() {
+    $("#level-title").html("Level " + gamePattern.length);
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColour = buttonColours[randomNumber];
 
-    simonPattern.push(random);
-    level.html("Level " + simonPattern.length);
+    playSound(randomChosenColour);
+    animatePress(randomChosenColour);
 
-    switch (random) {
-        case 1:
-            green.addClass("pressed");
-            greenAudio.play();
-            setTimeout(() => {
-                green.removeClass("pressed");
-            }, 100);
-            break;
-        case 2:
-            red.addClass("pressed");
-            redAudio.play();
-            setTimeout(() => {
-                red.removeClass("pressed");
-            }, 100);
-            break;
-        case 3:
-            yellow.addClass("pressed");
-            yellowAudio.play();
-            setTimeout(() => {
-                yellow.removeClass("pressed");
-            }, 100);
-            break;
-        case 4:
-            blue.addClass("pressed");
-            blueAudio.play();
-            setTimeout(() => {
-                blue.removeClass("pressed");
-            }, 100);
-            break;
-        default:
-            break;
-    }
-    // console.log("random " + random);
+    gamePattern.push(randomChosenColour);
 }
 
-function check() {
-    for (let i = 0; i <= counter; i++) {
-        if (playerPattern[i] != simonPattern[i]) {
+function playSound(name) {
+    audio = new Audio("./sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour) {
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(() => {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer() {
+    for (let i = 0; i <= userClickedPattern.length - 1; i++) {
+        if (userClickedPattern[i] != gamePattern[i]) {
             return false;
         }
     }
     return true;
 }
 
-green.on("click", () => {
-    playerPattern.push(1);
-
-    if (check() == true) {
-        counter++;
-        green.addClass("pressed");
-        setTimeout(() => {
-            green.removeClass("pressed");
-        }, 100);
-        setTimeout(() => {
-            if (counter == simonPattern.length) {
-                pick();
-                counter = 0;
-                playerPattern = [];
-            }
-        }, 200);
-        greenAudio.play();
+$(".btn").on("click", (e) => {
+    let userChosenColour = e.currentTarget.id;
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    userClickedPattern.push(userChosenColour);
+    if (checkAnswer() == true) {
+        if (userClickedPattern.length == gamePattern.length) {
+            setTimeout(() => {
+                nextSequence();
+            }, 500);
+            userClickedPattern = [];
+        }
     } else {
-        wrongAudio.play();
-        alert("WRONG");
-        playerPattern = [];
-        simonPattern = [];
-    }
-});
-
-red.on("click", () => {
-    playerPattern.push(2);
-
-    if (check() == true) {
-        counter++;
-        red.addClass("pressed");
+        play = false;
+        $("#level-title").html("Game Over, Press Any Key to Restart");
+        $("body").addClass("game-over");
         setTimeout(() => {
-            red.removeClass("pressed");
-        }, 100);
-        setTimeout(() => {
-            if (counter == simonPattern.length) {
-                pick();
-                counter = 0;
-                playerPattern = [];
-            }
+            $("body").removeClass("game-over");
         }, 200);
-        redAudio.play();
-    } else {
-        wrongAudio.play();
-        alert("WRONG");
-        playerPattern = [];
-        simonPattern = [];
-    }
-});
-
-yellow.on("click", () => {
-    playerPattern.push(3);
-
-    if (check() == true) {
-        counter++;
-        yellow.addClass("pressed");
-        setTimeout(() => {
-            yellow.removeClass("pressed");
-        }, 100);
-        setTimeout(() => {
-            if (counter == simonPattern.length) {
-                pick();
-                counter = 0;
-                playerPattern = [];
-            }
-        }, 200);
-        yellowAudio.play();
-    } else {
-        wrongAudio.play();
-        alert("WRONG");
-        playerPattern = [];
-        simonPattern = [];
-    }
-});
-
-blue.on("click", () => {
-    playerPattern.push(4);
-
-    if (check() == true) {
-        counter++;
-        blue.addClass("pressed");
-        setTimeout(() => {
-            blue.removeClass("pressed");
-        }, 100);
-        setTimeout(() => {
-            if (counter == simonPattern.length) {
-                pick();
-                counter = 0;
-                playerPattern = [];
-            }
-        }, 200);
-        blueAudio.play();
-    } else {
-        wrongAudio.play();
-        alert("WRONG");
-        playerPattern = [];
-        simonPattern = [];
+        wrong = new Audio("./sounds/wrong.mp3");
+        wrong.play();
     }
 });
